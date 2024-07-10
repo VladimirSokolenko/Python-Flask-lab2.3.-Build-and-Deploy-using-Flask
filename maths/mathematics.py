c0c4ba911c65ed053d0fcdcb3eafd83739f5fc32
vladimir.sokolenko <vladimir.sokolenko@gmail.com>
2024-07-10 13:53:32 +0300

    updated

--- maths/mathematics.py
+++ maths/mathematics.py
@@ -1,8 +1,12 @@
 ''' Basic math functions '''
 def summation (num1, num2):
+    ''' Summ '''
     return num1 + num2
 
-
+def subtraction (num1, num2):
+    ''' Difference '''
+    return num1 - num2
 
 def multiplication  (num1, num2):
+    ''' Product '''
     return num1 * num2
